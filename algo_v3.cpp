be3483e00e18a84ac9f65641b895f1edcd216129
shahryarsaljoughi <s.shahryar75@gmail.com>
2017-07-23 12:04:11 +0430

    big fixed after lots of time and effortgit status!

--- algo_v3.cpp
+++ algo_v3.cpp
@@ -63,17 +63,28 @@ void remove_inside_points(list<point> points, const point &l,const point &r){
 	for(point p: internal_points) points.remove(p);
 }
 
-void find_next_vertices(const list<point> points, point &l, point &r){
+void find_next_vertices(const list<point> points, point &l, point &r, const point& p_top, bool right_reached, bool left_reached){
 	cout<<"find_next_vertices is running"<<endl;
 	
-	list<point>::const_iterator it = points.begin();
-	point next_l = *it;
-	point next_r = *it;
+	list<point>::const_iterator it_l = points.begin();
+	list<point>::const_iterator it_r = points.begin();
+	
+	
+	while(!left_reached and(*it_l==l or (it_l->y) <l.y) and it_l!=points.end() and *it_l!=p_top) it_l++;
+	while(!right_reached and (*it_r==r or (it_r->y) <r.y) and it_r!=points.end() and *it_r!=p_top) it_r++;
+	
+	
+	point next_l = *it_l;
+	point next_r = *it_r;
 	
 	for(point p: points){
-		if ((p!=l) and (p.y>=l.y) and (polar_angle(l, p)<M_PI)and (polar_angle(l, p)>polar_angle(l, next_l))) next_l = p;
+		if ((p!=l) and (p.y>=l.y) and (polar_angle(l, p)<=M_PI)and 
+				(polar_angle(l, p)>polar_angle(l, next_l))) next_l = p;
 		if ((p!=r) and (p.y>=r.y) and (polar_angle(r, p)<polar_angle(r, next_r))) next_r = p;
 	}
+	
+	if(right_reached) next_r= p_top;
+	if(left_reached) next_l= p_top;
 	
 	l=next_l;
 	r=next_r;
@@ -95,21 +106,34 @@ vector<point> my_algo_v3(vector<point> &input){
 	point p_top = get_top(input);
 	
 	vertices.push_back(P0);
+	vertices.push_back(p_top);
 	right_vertex = left_vertex = P0;
 	points.remove(P0);
 	
 	cout<<"P0 is ("<<P0.x<<" , "<<P0.y<<") "<<endl;
 	
+	
+	bool right_reached = false;
+	bool left_reached = false;
+	
 	while(left_vertex != p_top or right_vertex != p_top){
 		cout<<"while is running"<<endl;
-		find_next_vertices(points, left_vertex, right_vertex);	
-		vertices.push_back(left_vertex);
-		vertices.push_back(right_vertex);
+		find_next_vertices(points, left_vertex, right_vertex, p_top, right_reached, left_reached);	
+		
+		if (left_vertex==p_top) left_reached = true;
+		if (right_vertex==p_top) right_reached = true;
+		
+		if (left_vertex!=p_top) vertices.push_back(left_vertex);
+		if (right_vertex!=p_top) vertices.push_back(right_vertex);
+		
+		
+		
+		
 		remove_inside_points(points, left_vertex, right_vertex);
 		cout<<"("<<right_vertex.x<<","<<right_vertex.y<<") found on right side"<<endl;
 		cout<<"("<<left_vertex.x<<","<<left_vertex.y<<") found on left side"<<endl;
-		points.remove(left_vertex);
-		points.remove(right_vertex);
+		if (left_vertex!=p_top) points.remove(left_vertex);
+		if (right_vertex!=p_top)points.remove(right_vertex);
 	}
 	
 	return vertices;
